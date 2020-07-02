40c7c85d5c88873bd5e3078ba307b1a88471d11c
AvichaiV <avichaiv@gmail.com>
2020-07-02 13:22:06 +0300

    remove comments

--- stop.sh
+++ stop.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-#docker-compose -f mysql.yml down
-#docker stop final1
-#docker rm final1
-#docker container prune -f
-#docker image prune -f
-
 echo "Do you want to Stop the SQL Docker (y/n) ?"
 read stopsql
 if [ "$stopsql" == y ]; 
